31768277b02a3d9d010211bb27ae465ed55ad97d
PoornaGTX <onlinepoorna@gmail.com>
2022-05-02 08:44:43 +0530

    server implementaion

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ import express from "express";
 const app = express();
 import dotenv from "dotenv";
 dotenv.config();
+import "express-async-errors";
 
+// db and authenticateUser
 import connectDB from "./db/connect.js";
+
+// routers
+import authRouter from "./routes/authRoutes.js";
+import jobsRouter from "./routes/jobsRouter.js";
 
 // middleware
 import errorHandlerMiddleware from "./middleware/error-handler.js";
@@ -11,10 +17,15 @@ import notFoundMiddleware from "./middleware/not-found.js";
 
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   throw new Error("error");
   res.send("Welcome");
 });
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
