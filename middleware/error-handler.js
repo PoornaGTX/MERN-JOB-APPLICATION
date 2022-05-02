06f5919ffd965ff90c96e9dbfa26740f8c251c28
PoornaGTX <onlinepoorna@gmail.com>
2022-05-02 10:16:50 +0530

    Error Handling

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,12 +1,29 @@
 import { StatusCodes } from "http-status-codes";
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  console.log(err);
+  console.log(err.message);
+
   const defaultError = {
-    StatusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-    msg: "Something went wrong try again later",
+    StatusCode: err.StatusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong, try again later",
   };
-  res.status(defaultError.StatusCode).json({ msg: err });
+
+  if (err.name === "ValidationError") {
+    defaultError.StatusCode = StatusCodes.BAD_REQUEST;
+    //defaultError.msg = err.message;
+    defaultError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+  }
+
+  //check eamil that already exsists
+  if (err.code && err.code === 11000) {
+    defaultError.StatusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique`;
+  }
+
+  //res.status(defaultError.StatusCode).json({ msg: err });
+  res.status(defaultError.StatusCode).json({ msg: defaultError.msg });
 };
 
 export default errorHandlerMiddleware;
