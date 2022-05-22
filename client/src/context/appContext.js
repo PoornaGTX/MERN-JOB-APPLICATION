416278efb98443293bc71f1ed3f3e4fcc912d032
PoornaGTX <onlinepoorna@gmail.com>
2022-05-22 16:55:11 +0530

    add job

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -18,6 +18,7 @@ import {
   UPDATE_USER_BEGIN,
   UPDATE_USER_SUCCESS,
   UPDATE_USER_ERROR,
+  HANDLE_CHANGE,
 } from "./action";
 
 const token = localStorage.getItem("token");
@@ -206,6 +207,10 @@ const AppProvider = ({ children }) => {
     clearAlert();
   };
 
+  const handleChange = ({ name, value }) => {
+    dispatch({ type: HANDLE_CHANGE, payload: { name, value } });
+  };
+
   return (
     <AppContext.Provider
       value={{
@@ -217,6 +222,7 @@ const AppProvider = ({ children }) => {
         toggleSidebar,
         logoutUser,
         updateUser,
+        handleChange,
       }}
     >
       {children}
